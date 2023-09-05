6b3b5d621d0f9710c97f4ba8d00ba9199febc033
fangchuan.tan <tfcode@163.com>
2023-09-05 16:33:14 +0800

    test

--- src/main/java/org/springframework/beans/factory/BeanFactory.java
+++ src/main/java/org/springframework/beans/factory/BeanFactory.java
@@ -10,27 +10,27 @@ import org.springframework.beans.BeansException;
  */
 public interface BeanFactory {
 
-	/**
-	 * 获取bean
-	 *
-	 * @param name
-	 * @return
-	 * @throws BeansException bean不存在时
-	 */
-	Object getBean(String name) throws BeansException;
+    /**
+     * 获取bean
+     *
+     * @param name
+     * @return
+     * @throws BeansException bean不存在时
+     */
+    Object getBean(String name) throws BeansException;
 
-	/**
-	 * 根据名称和类型查找bean
-	 *
-	 * @param name
-	 * @param requiredType
-	 * @param <T>
-	 * @return
-	 * @throws BeansException
-	 */
-	<T> T getBean(String name, Class<T> requiredType) throws BeansException;
+    /**
+     * 根据名称和类型查找bean
+     *
+     * @param name
+     * @param requiredType
+     * @param <T>
+     * @return
+     * @throws BeansException
+     */
+    <T> T getBean(String name, Class<T> requiredType) throws BeansException;
 
-	<T> T getBean(Class<T> requiredType) throws BeansException;
+    <T> T getBean(Class<T> requiredType) throws BeansException;
 
-	boolean containsBean(String name);
+    boolean containsBean(String name);
 }
